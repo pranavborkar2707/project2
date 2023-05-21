7c1f5218ad275dbf7d3d9d5a39ed564704488f5e
Pranav <pranavborkar1999@gmail.com>
2023-05-21 20:53:33 +0530

    Addition Of Two Number Program

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Add from './components/Add/Add';
 
 function App() {
+
+  const[value1,setvalue1]=useState(0);
+  const inputvalue=() =>{
+    return value1;
+  }
+  const[value2,setvalue2]=useState(0);
+  const inputvalue2 =() =>{
+    return value2;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h4>Addition Of Two Numbers</h4>
+    <input 
+    value={value1}
+    onChange={(m)=>{
+      setvalue1(m.target.value)
+      console.log('value1 = ', value1)
+    }}
+     type='number' placeholder='enter value' /><br></br><br></br>
+    
+    <input 
+    value={value2}
+    onChange={(f)=>{
+      setvalue2(f.target.value)
+      console.log('inputvalue2',value2)
+    }}
+    type='number' placeholder='enter value' /><br></br><br></br>
+
+    <Add a={value1} b={value2}></Add>
+
+    <button>Enter</button>
+  </div>
+
+    
+
   );
 }
 
---
+++ src/components/Add/Add.js
@@ -0,0 +1,14 @@
+const Add=(props)=>{
+   return(
+   
+   <div>
+   <h3>This is a Add components</h3>
+   <p>Total={parseInt(props.a)+parseInt(props.b)}</p>
+  
+   </div>
+   );
+   
+
+}
+
+export default Add;
